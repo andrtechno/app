ad0ddbb74ff0e48e81b1837b24c5fe6c855c5f0b
Andrew <andrew.panix@gmail.com>
2019-03-14 08:28:23 +0200

    111

--- backend/themes/dashboard/views/main/error.php
+++ backend/themes/dashboard/views/main/error.php
@@ -1,6 +1,5 @@
 <?php
 
-use yii\helpers\Html;
 use panix\engine\bootstrap\Alert;
 
 $this->title = $name;
@@ -16,27 +15,27 @@ echo Alert::widget([
 ]);
 ?>
 <?php
-if(YII_DEBUG){
+if (YII_DEBUG) {
 
-foreach ($exception->getTrace() as $trace) { ?>
-    <?php
-    Alert::begin([
-        'options' => [
-            'class' => 'alert-info',
-        ],
-        'closeButton' => false
-    ]);
-    ?>
+    foreach ($exception->getTrace() as $trace) { ?>
+        <?php
+        Alert::begin([
+            'options' => [
+                'class' => 'alert-info',
+            ],
+            'closeButton' => false
+        ]);
+        ?>
 
-    <div class="">Файл: <?= $trace['file'] ?></div>
-    <div class="">Строка: <?= $trace['line'] ?></div>
-    <div class="">Функция: <?= $trace['function'] ?></div>
-    <div class="">class: <?= $trace['class'] ?></div>
-    <?php foreach ($trace['args'] as $args) { ?>
-        <?php //print_r($args); ?>
-    <?php } ?>
+        <div class="">Файл: <?= $trace['file'] ?></div>
+        <div class="">Строка: <?= $trace['line'] ?></div>
+        <div class="">Функция: <?= $trace['function'] ?></div>
+        <div class="">class: <?= $trace['class'] ?></div>
+        <?php foreach ($trace['args'] as $args) { ?>
+            <?php //print_r($args); ?>
+        <?php } ?>
 
-    <?php Alert::end(); ?>
+        <?php Alert::end(); ?>
 
-<?php }
+    <?php }
 }
