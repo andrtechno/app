fdd0b36d2c435b28666663829432824b0463d22c
Andrew <andrew.panix@gmail.com>
2019-03-16 18:47:55 +0200

    111

--- config/db.php
+++ config/db.php
@@ -2,9 +2,12 @@
 
 return [
     'class' => 'panix\engine\db\Connection',
+   // 'dsn'=>'mysql:host=pix1.mysql.tools;dbname=pix1_v2',
+   // 'username'=>'pix1_v2',
+   // 'password'=>'uev8f9g8',
     'dsn'=>'mysql:host=localhost;dbname=yii2',
     'username'=>'root',
-    'password'=>'',
+    'password'=>'47228960panix',
     'charset' => 'utf8',
     //'on afterOpen' => function($event) {
         //$event->sender->createCommand("SET time_zone = '" . date('P') . "'")->execute();
@@ -13,7 +16,7 @@ return [
     //'charset' => 'utf8mb4', //utf8_general_ci на utf8mb4_general_ci. FOR Emoji
     'tablePrefix' => 'cms_',
     'serverStatusCache' => YII_DEBUG ? 0 : 3600,
-    'schemaCacheDuration' => YII_DEBUG ? 0 : 3600,
-    'enableSchemaCache' => YII_DEBUG,
+    'schemaCacheDuration' => YII_DEBUG ? 0 : 3600*24,
+    'enableSchemaCache' => true,
     'schemaCache' => 'cache',
 ];
--- config/db_local.php
+++ config/db_local.php
@@ -4,7 +4,7 @@ return [
     'class' => 'panix\engine\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=yii2',
     'username' => 'root',
-    'password' => '',
+    'password' => '47228960panix',
     //'charset'=>'utf8',
     'charset' => 'utf8mb4', //utf8_general_ci на utf8mb4_general_ci. FOR Emoji
     //'on afterOpen' => function($event) {
@@ -13,8 +13,8 @@ return [
     //},
     'tablePrefix' => 'cms_',
     'serverStatusCache' => YII_DEBUG ? 0 : 3600,
-    'schemaCacheDuration' => YII_DEBUG ? 0 : 3600,
-    'enableSchemaCache' => YII_DEBUG,
+    'schemaCacheDuration' => YII_DEBUG ? 0 : 3600*24,
+    'enableSchemaCache' => true,
     'schemaCache' => 'cache',
 
 ];
