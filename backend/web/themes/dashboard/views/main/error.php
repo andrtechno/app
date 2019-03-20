85e34f0540e4390e1e3467182726eeaec3d53ea1
Andrew <andrew.panix@gmail.com>
2019-03-20 16:36:30 +0200

    111

--- backend/web/themes/dashboard/views/main/error.php
+++ backend/web/themes/dashboard/views/main/error.php
@@ -1,8 +1,6 @@
 <?php
 
-use panix\engine\bootstrap\Alert;
 use yii\helpers\Html;
-
 
 ?>
 
@@ -12,25 +10,15 @@ use yii\helpers\Html;
             <h1><?= $exception->statusCode; ?></h1>
             <h2><?= $exception->getMessage(); ?></h2>
             <p>
-                <?= Html::a('На главную', ['/'], ['class' => 'btn btn-primary']); ?>
+                <?= Html::a(Yii::t('app', 'GO_HOME'), ['/'], ['class' => 'btn btn-primary']); ?>
             </p>
         </div>
     </div>
     <div class="col-12">
-        <?php
-        /*Alert::begin([
-            'options' => [
-                'class' => 'alert-dange2r',
-            ],
-            'closeButton' => false
-        ]);*/
-        ?>
         <?php foreach ($exception->getTrace() as $trace) { ?>
             <div class=""><strong><?= $trace['file'] ?></strong>(<?= $trace['line'] ?>)</div>
             <div class=""><?= $trace['class'] ?>::<?= $trace['function'] ?></div>
             <hr/>
         <?php } ?>
-        <?php //Alert::end(); ?>
-
     </div>
 </div>
