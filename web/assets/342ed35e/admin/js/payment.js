ffdbd62819e0b9b227d21acaf6626f56b94ac026
Andrew <andrew.panix@gmail.com>
2019-04-26 05:06:50 +0300

    init

--- web/assets/342ed35e/admin/js/payment.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    var sel = $('#payment-payment_system');
-    sel.on('change', function () {
-        $('#payment_configuration').load('/admin/cart/payment/render-configuration-form?system=' + $(this).val() + '&payment_method_id=' + $(this).attr('rel'));
-    });
-    sel.change();
-});
