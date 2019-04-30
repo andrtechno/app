1513b59304bcabdcdd9a5ad8e1ad4dc3d1e24f77
Andrew <andrew.panix@gmail.com>
2019-04-30 13:39:35 +0300

    init

--- migrations/m171205_122127_session.php
+++ migrations/m171205_122127_session.php
@@ -18,17 +18,17 @@ class m171205_122127_session extends Migration
     public function up()
     {
         $this->createTable($this->tableName, [
-            'id' => \yii\db\Schema::TYPE_CHAR . '(40) not null',
+            'id' => \yii\db\Schema::TYPE_CHAR . '(40) NOT NULL',
             'user_id' => $this->integer()->null()->unsigned(),
             'expire' => $this->integer()->notNull(),
             'expire_start' => $this->integer()->notNull(),
             'data' => 'BLOB',
             'ip' => $this->string(100),
-            'user_type' =>  $this->string(50)->notNull(),
-            'user_name' =>  $this->string(100)->notNull()
+            'user_type' => $this->string(50)->notNull(),
+            'user_name' => $this->string(100)->notNull()
         ]);
-        $this->createIndex('user_id', $this->tableName, 'user_id');
-        $this->createIndex('expire', $this->tableName, 'expire');
+        //$this->createIndex('id', $this->tableName, 'id');
+        $this->addPrimaryKey('id',$this->tableName,'id');
     }
 
     public function down()
