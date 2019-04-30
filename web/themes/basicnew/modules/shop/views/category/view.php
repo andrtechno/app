1008167a781532999f4a7a9159fded64d8b5b758
Andrew <andrew.panix@gmail.com>
2019-04-30 18:03:13 +0300

    init

--- web/themes/basicnew/modules/shop/views/category/view.php
+++ web/themes/basicnew/modules/shop/views/category/view.php
@@ -18,19 +18,19 @@ use panix\mod\shop\widgets\filtersnew\FiltersWidget;
                 <span>&times;</span>
             </a>
 
-        <?php
-        echo FiltersWidget::widget([
-            'model' => $this->context->dataModel,
-            'attributes' => $this->context->eavAttributes,
+            <?php
+            echo FiltersWidget::widget([
+                'model' => $this->context->dataModel,
+                'attributes' => $this->context->eavAttributes,
 
-        ]);
+            ]);
 
-        ?>
+            ?>
         </div>
     </div>
     <div class="catalog-content">
         <div class="heading-gradient">
-        <h1><?= $this->context->pageName; ?></h1>
+            <h1><?= Html::encode(($this->h1) ? $this->h1 : $this->context->pageName); ?></h1>
         </div>
         <?php echo $this->render('_sorting', ['itemView' => $itemView]); ?>
 
