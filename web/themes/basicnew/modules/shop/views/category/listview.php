af29ef830fd3f46d393ee9f269c4ba3ce8930dc6
Andrew <andrew.panix@gmail.com>
2019-05-07 15:04:28 +0300

    init

--- web/themes/basicnew/modules/shop/views/category/listview.php
+++ web/themes/basicnew/modules/shop/views/category/listview.php
@@ -1,5 +1,8 @@
 <?php
-
+/**
+ * @var $provider
+ * @var $itemView
+ */
 echo \yii\widgets\ListView::widget([
     //'id'=>'list-product',
     'dataProvider' => $provider,
@@ -7,7 +10,7 @@ echo \yii\widgets\ListView::widget([
     //'layout' => '{sorter}{summary}{items}{pager}',
     'layout' => '{items}{pager}',
     'emptyText' => 'Empty',
-    'options' => ['class' => 'list-view '.$itemView],
+    'options' => ['class' => 'list-view clearfix2 '.$itemView],
     'itemOptions' => ['class' => 'item'],
     'sorter' => [
         //'class' => \yii\widgets\LinkSorter::class,
@@ -15,8 +18,8 @@ echo \yii\widgets\ListView::widget([
     ],
     'pager' => [
         'class' => \panix\wgt\scrollpager\ScrollPager::class,
-        'triggerTemplate' => '<div class="ias-trigger" style="text-align: center; cursor: pointer;width: 100%;">{text}</div>',
-        'spinnerTemplate' => '<div class="ias-spinner" style="text-align: center;width: 100%;"><img src="{src}" alt="" /></div>',
+       // 'triggerTemplate' => '<div class="ias-trigger" style="text-align: center; cursor: pointer;width: 100%;">{text}</div>',
+       // 'spinnerTemplate' => '<div class="ias-spinner" style="text-align: center;width: 100%;"><img src="{src}" alt="" /></div>',
         'spinnerSrc'=>$this->context->assetUrl.'/images/ajax.gif'
 
     ],
