d80c4d8a32a37b1f25b237d17d7c9b2dcfc4c1ed
Andrew <andrew.panix@gmail.com>
2019-05-07 16:37:08 +0300

    init

--- web/themes/basicnew/modules/shop/views/category/listview.php
+++ web/themes/basicnew/modules/shop/views/category/listview.php
@@ -18,8 +18,8 @@ echo \yii\widgets\ListView::widget([
     ],
     'pager' => [
         'class' => \panix\wgt\scrollpager\ScrollPager::class,
-       // 'triggerTemplate' => '<div class="ias-trigger" style="text-align: center; cursor: pointer;width: 100%;">{text}</div>',
-       // 'spinnerTemplate' => '<div class="ias-spinner" style="text-align: center;width: 100%;"><img src="{src}" alt="" /></div>',
+        'triggerTemplate' => '<div class="ias-trigger" style="text-align: center; cursor: pointer;width: 100%;">{text}</div>',
+        'spinnerTemplate' => '<div class="ias-spinner" style="text-align: center;width: 100%;"><img src="{src}" alt="" /></div>',
         'spinnerSrc'=>$this->context->assetUrl.'/images/ajax.gif'
 
     ],
