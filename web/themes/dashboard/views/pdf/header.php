9692b20bddd43adc6e2f38169c5bead681849fdd
Andrew <andrew.panix@gmail.com>
2019-06-15 09:21:17 +0300

    init

--- web/themes/dashboard/views/pdf/header.php
+++ web/themes/dashboard/views/pdf/header.php
@@ -1,11 +1,11 @@
-<h1>dasdsa</h1>
+<h1><?= $title; ?></h1>
 <div class="container">
-<div class="row">
-    <div class="col-md-6">
-asd
+    <div class="row">
+        <div class="col-md-6">
+            col 1
+        </div>
+        <div class="col-md-6">
+            col 2
+        </div>
     </div>
-    <div class="col-md-6">
-asds
-    </div>
-</div>
 </div>
