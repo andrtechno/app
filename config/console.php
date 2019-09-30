4434deb6cb8e17b3619de369f4a1d9dff3a94964
Andrew <andrew.panix@gmail.com>
2019-09-30 16:41:07 +0300

    init

--- config/console.php
+++ config/console.php
@@ -5,7 +5,7 @@ return [
     'name' => 'PIXELION CMS',
     'basePath' => dirname(__DIR__),
     //'basePath' => dirname(__DIR__),
-    'bootstrap' => ['log','telegram','queue','panix\engine\BootstrapModule'],
+    'bootstrap' => ['log', 'queue', 'panix\engine\BootstrapModule'], //'telegram',
     'controllerNamespace' => 'app\commands',
     'language' => 'ru',
     'aliases' => [
@@ -26,12 +26,19 @@ return [
                 '@panix/engine/plugins',
             ]
         ],
-        'telegram' => [
+        /*'telegram' => [
             'class' => 'panix\mod\telegram\Module',
             'hook_url' => 'https://yii2.pixelion.com.ua/telegram/default/hook', // must be https! (if not prettyUrl https://yourhost.com/index.php?r=telegram/default/hook)
             // 'db' => 'db2', //db file name from config dir
             'userCommandsPath' => '@telegram/commands/UserCommands',
             // 'timeBeforeResetChatHandler' => 60
+        ],*/
+
+        'rbac' => [
+            'class' => 'panix\mod\rbac\ConsoleModule',
+            //'as access' => [
+            //    'class' => panix\mod\rbac\filters\AccessControl::class
+            //],
         ],
         'banner' => ['class' => 'panix\mod\banner\Module'],
         //'rbac' => [
@@ -49,10 +56,13 @@ return [
         ],
         'migrate' => ['class' => 'panix\engine\console\controllers\MigrateController',
             //'migrationPath' => '@console/migrations',
-            // 'migrationNamespaces' => [
-            //  'console\migrations',
-            // 'lo\plugins\migrations',
-            // ],
+            'migrationNamespaces' => [
+                //  'console\migrations',
+                // 'lo\plugins\migrations',
+                //'app\migrations',
+                // 'yii\rbac\migrations'
+            ],
+            'migrationPath' => ['@app/migrations', '@yii/rbac/migrations'],
         ]
     ],
     'components' => [
@@ -63,8 +73,11 @@ return [
         ],
         //'session' => [
         //    'class' => 'yii\web\Session'
-        // ],
-
+        //],
+        'authManager' => [
+            'class' => 'yii\rbac\DbManager',
+            'defaultRoles' => ['guest', 'user'],
+        ],
         'urlManager' => require(__DIR__ . '/urlManager.php'),
         'settings' => ['class' => 'panix\engine\components\Settings'],
         'cache' => ['class' => 'yii\caching\FileCache'],
@@ -73,29 +86,29 @@ return [
         'db' => require(__DIR__ . '/../config/db_local.php'),
         'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
-            'flushInterval'=>1000*10,
+            'flushInterval' => 1000 * 10,
             'targets' => [
-                'file1'=>[
+                'file1' => [
                     'class' => 'yii\log\FileTarget',
                     'levels' => ['error', 'warning'],
                     'logVars' => [],
                     'logFile' => '@runtime/logs/' . date('Y-m-d') . '/db_error.log',
                     'categories' => ['yii\db\*']
                 ],
-                'file2'=>[
+                'file2' => [
                     'class' => 'yii\log\FileTarget',
                     'levels' => ['error', 'warning'],
                     'logVars' => [],
                     'logFile' => '@runtime/logs/' . date('Y-m-d') . '/error.log',
                     // 'categories' => ['yii\db\*']
                 ],
-                'file3'=>[
+                'file3' => [
                     'class' => 'yii\log\FileTarget',
                     'levels' => ['warning'],
                     'logVars' => [],
                     'logFile' => '@runtime/logs/' . date('Y-m-d') . '/warning.log',
                 ],
-                'file4'=>[
+                'file4' => [
                     'class' => 'yii\log\FileTarget',
                     'levels' => ['info'],
                     'logVars' => [],
