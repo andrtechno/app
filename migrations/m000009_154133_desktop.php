947494c2dfca920bcf76ddc34df28d4864963a86
Andrew <andrew.panix@gmail.com>
2019-10-17 20:05:55 +0300

    init

--- migrations/m000009_154133_desktop.php
+++ migrations/m000009_154133_desktop.php
@@ -10,6 +10,7 @@
 
 use panix\engine\db\Migration;
 use panix\mod\admin\models\Desktop;
+use panix\mod\admin\models\DesktopWidgets;
 
 class m000009_154133_desktop extends Migration
 {
@@ -25,15 +26,26 @@ class m000009_154133_desktop extends Migration
             'columns' => $this->tinyInteger(3),
             'private' => $this->tinyInteger(1)->defaultValue(0),
             'created_at' => $this->timestamp()->defaultValue(null),
-            'updated_at' => $this->timestamp()->defaultValue(null)
+            'updated_at' => $this->timestamp()->defaultValue(null),
+            'ordern' => $this->integer()->unsigned(),
+        ]);
+
+        $this->createTable(DesktopWidgets::tableName(), [
+            'id' => $this->primaryKey()->unsigned(),
+            'desktop_id' => $this->integer()->unsigned()->notNull(),
+            'widget' => $this->string(100)->notNull(),
+            'col' => $this->tinyInteger(1)->defaultValue(1),
+            'title' => $this->string(255)->notNull(),
         ]);
 
         $this->createIndex('user_id', Desktop::tableName(), 'user_id');
+        $this->createIndex('desktop_id', DesktopWidgets::tableName(), 'desktop_id');
     }
 
     public function down()
     {
         $this->dropTable(Desktop::tableName());
+        $this->dropTable(DesktopWidgets::tableName());
     }
 
 }
