92b2bf326bac9e8b771786ce0bdc98bf9a25fe40
Andrew <andrew.panix@gmail.com>
2019-11-13 10:59:37 +0200

    init

--- migrations/m000004_202402_grid_columns.php
+++ migrations/m000004_202402_grid_columns.php
@@ -5,36 +5,39 @@
  *
  * @author PIXELION CMS development team <dev@pixelion.com.ua>
  * @link http://pixelion.com.ua PIXELION CMS
- * 
+ *
  * Class m000004_202402_grid_columns
  */
 
 use panix\engine\db\Migration;
+use panix\engine\grid\GridColumns;
 
-class m000004_202402_grid_columns extends Migration {
-
-    public $tableName = '{{%grid_columns}}';
+class m000004_202402_grid_columns extends Migration
+{
 
     /**
      * @inheritdoc
      */
-    public function up() {
-        $this->createTable($this->tableName, [
+    public function up()
+    {
+        $this->createTable(GridColumns::tableName(), [
             'id' => $this->primaryKey()->unsigned(),
             'grid_id' => $this->string(25)->notNull(),
             'modelClass' => $this->string(255)->notNull(),
-            'column_key' => $this->string(25)->notNull(),
-            'ordern' => $this->integer(),
+            'column_data' => $this->text()->notNull(),
+            'pageSize' => $this->smallInteger()->null(),
         ]);
-        $this->createIndexes(['modelClass', 'column_key', 'ordern','grid_id']);
-        
+
+        $this->createIndex('modelClass', GridColumns::tableName(), 'modelClass');
+        $this->createIndex('grid_id', GridColumns::tableName(), 'grid_id');
     }
 
     /**
      * @inheritdoc
      */
-    public function down() {
-        $this->dropTable($this->tableName);
+    public function down()
+    {
+        $this->dropTable(GridColumns::tableName());
     }
 
 }
