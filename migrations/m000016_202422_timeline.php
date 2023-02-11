94e9e798917675f92813323449defca8c542bca8
Andrew <andrew.panix@gmail.com>
2023-02-11 05:43:10 +0300

    Create m000016_202422_timeline.php

---
+++ migrations/m000016_202422_timeline.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Generation migrate by PIXELION CMS
+ * @author PIXELION CMS development team <dev@pixelion.com.ua>
+ *
+ * Class m000016_202422_timeline
+ */
+
+use panix\engine\db\Migration;
+use panix\mod\admin\models\Timeline;
+
+
+class m000016_202422_timeline extends Migration
+{
+
+    public function up()
+    {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+
+        $this->createTable(Timeline::tableName(), [
+            'id' => $this->primaryKey()->unsigned(),
+            'user_id' => $this->integer()->null(),
+            'created_at' => $this->integer()->null(),
+        ], $tableOptions);
+
+    }
+
+    public function down()
+    {
+        $this->dropTable(Timeline::tableName());
+    }
+
+}
