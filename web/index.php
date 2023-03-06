eb9015226e5c02ba9597a741b81af86936bc31e6
Andrew <andrew.panix@gmail.com>
2023-03-06 18:37:07 +0300

    Update index.php

--- web/index.php
+++ web/index.php
@@ -6,15 +6,11 @@ date_default_timezone_set("UTC");
 
 // comment out the following two lines when deployed to production
 if (in_array($_SERVER['REMOTE_ADDR'], ['127.0.0.1'])) {
-    $env = 'dev';
-    $debug = true;
-} else {
-    $env = 'prod';
-    $debug = false;
+    defined('YII_DEBUG') or define('YII_DEBUG', true);
+    defined('YII_ENV') or define('YII_ENV', 'dev');
 }
 
-defined('YII_DEBUG') or define('YII_DEBUG', $debug);
-defined('YII_ENV') or define('YII_ENV', $env);
+
 
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 require(__DIR__ . '/../vendor/autoload.php');
