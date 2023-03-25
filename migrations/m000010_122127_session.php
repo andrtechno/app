7ca830a91e67a7af6021416f9b4ebc603c9f2f52
Andrew <andrew.panix@gmail.com>
2023-03-26 02:15:15 +0300

    Update m000010_122127_session.php

--- migrations/m000010_122127_session.php
+++ migrations/m000010_122127_session.php
@@ -20,7 +20,7 @@ class m000010_122127_session extends Migration
             'user_id' => $this->integer()->null()->unsigned(),
             'expire' => $this->integer()->null(),
             'expire_start' => $this->integer()->null(),
-            'data' => 'LONGBLOB',
+            'data' => ($this->db->driverName === 'pgsql') ? 'bytea':'LONGBLOB',
             'ip' => $this->string(100),
             'user_type' => $this->string(50)->null(),
             'user_name' => $this->string(100)->null(),
